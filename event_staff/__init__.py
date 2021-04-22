920a9d67de1f886c674f39c6e2f4d069bade7da9
Albin <albin@vidts.in>
2021-04-22 21:43:38 +0530

    event_staff Initial commit

---
+++ event_staff/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021-Today Albin John<albinjohn54@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import models
+from . import controllers
+from . import report
+from . import wizard
---
+++ event_staff/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021-Today Albin John<albinjohn54@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+	'name': 'Event Staff',
+	'category': 'Marketing/Events',
+	'author': 'Albin John, Odoo Community Association (OCA)',
+	'website':'https://github.com/oca/event.git',
+	'version': '13.0.1.0.0',
+	'depends': [
+		"event"
+	],
+
+	'description': """
+		Event Staff
+	""",
+	'data': [
+
+	],
+	'qweb': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': "AGPL-3"
+}
