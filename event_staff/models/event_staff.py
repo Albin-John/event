dfa3090a510bbb866642a9a659760a421efe4320
Albin <albin@vidts.in>
2021-04-23 23:59:36 +0530

    add the staff as confirmed attendees

--- event_staff/models/event_staff.py
+++ event_staff/models/event_staff.py
@@ -1,7 +1,7 @@
 # Copyright 2021-Today Albin John<albinjohn54@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EventStaff(models.Model):
@@ -12,3 +12,17 @@ class EventStaff(models.Model):
     role = fields.Many2one("event.staff.role", string="Role")
     event_id = fields.Many2one("event.event", string="Event", required=True)
     note = fields.Text()
+
+    @api.model
+    def create(self, vals):
+        event_staff_id = super(EventStaff, self).create(vals)
+        if event_staff_id:
+            attendee = self.env["event.registration"].create(
+                {
+                    "partner_id": event_staff_id.id,
+                    "event_id": event_staff_id.event_id.id,
+                }
+            )
+            attendee._onchange_partner()
+            attendee.confirm_registration()
+        return event_staff_id
